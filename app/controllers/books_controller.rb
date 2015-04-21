56802d13d45bf8283f1bf2ebd6b5557a72ccb37d
michelle <michelle@Razorblade.local>
2015-04-21 11:38:32 -0300

    Move logic from controller to User model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,60 +4,12 @@ class BooksController < ApplicationController
   end
 
   def index
-    if params[:search]
-      @user_db = User.where("name LIKE ?", "%#{params[:search]}%")
-    end 
-       
-    @user = @user_db.map do |x|
-      x.id
-    end
     
-    @user_name_array = @user_db.map do |x|
-      x.name
-    end
+    @user = User.search(params[:search])
 
-    @user_name = @user_name_array[0]
-
-    @user_favorite_books = Like.where(user_id: @user).map do |x|
+    @favorite_books = Like.where(user_id: @user.id).map do |x|
       Book.find(x.book_id)
     end
-
-    @likes_in_commom_user_and_other_users = @user_favorite_books.map do |x|
-      Like.where(book_id: x.id).map do |y|
-        y
-      end.flat_map {|i| i}
-    end
-
-    @similar_users = @likes_in_commom_user_and_other_users.flat_map do |i| 
-      i
-    end.map do |x|
-      x.user_id if x.user_id != @user
-    end
-
-    @similar_users_ids = []
-    @similar_users.map do |x|
-      if x != nil
-        @similar_users_ids << x
-      end
-    end
-    
-    @similar_users_likes = @similar_users_ids.map do |x|
-      Like.where(user_id: x).map do |y|
-        y
-      end
-    end.flat_map do |i| 
-      i
-    end
-
-    @similar_users_books = @similar_users_likes.map do |x|
-      Book.find(x.book_id).title
-    end
-
-    @user_books = @user_favorite_books.map do |title|
-      title.title
-    end
-
-    @recommendations = @similar_users_books - @user_books
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,55 @@
 class User < ActiveRecord::Base
 
-  # def self.search(query)
-  #   find_by_name("%#{query}%") 
-  # end
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%").first 
+  end
+
+  def recommended_books
+
+    @user_id = self.id
+
+    @favorite_books = Like.where(user_id: @user_id).map do |x|
+      Book.find(x.book_id)
+    end
+
+    @likes_in_commom_user_and_other_users = @favorite_books.map do |x|
+      Like.where(book_id: x.id).map do |y|
+        y
+      end.flat_map {|i| i}
+    end
+
+    @similar_users = @likes_in_commom_user_and_other_users.flat_map do |i| 
+      i
+    end.map do |x|
+      x.user_id if x.user_id != @user_id
+    end
+
+    @similar_users_ids = []
+    @similar_users.map do |x|
+      if x != nil
+        @similar_users_ids << x
+      end
+    end
+    
+    @similar_users_likes = @similar_users_ids.map do |x|
+      Like.where(user_id: x).map do |y|
+        y
+      end
+    end.flat_map do |i| 
+      i
+    end
+
+    @similar_users_books = @similar_users_likes.map do |x|
+      Book.find(x.book_id).title
+    end
+
+    @user_books = @favorite_books.map do |title|
+      title.title
+    end
+
+    @recommendations = @similar_users_books - @user_books
+
+  end
+
+
 end
